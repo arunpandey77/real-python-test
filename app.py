cb68dfede9f6273d541a2248c8309384ba3fdf09
Arun Pandey <arun.of.pandey@gmail.com>
2017-12-20 11:40:26 +0530

    flask-hello-world

---
+++ app.py
@@ -0,0 +1,9 @@
+#--- Flask Hello World ----#
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+@app.route("/hello")
+def hello_world():
+    return "hello world"
+if __name__ =="__main__":
+    app.run()
