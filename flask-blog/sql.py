f3feda9e34849b5cf7d00c229155abdccb61b535
Arun Pandey <arun.of.pandey@gmail.com>
2018-01-10 12:08:08 +0530

    'init'

--- flask-blog/sql.py
+++
@@ -1,8 +0,0 @@
-import sqlite3 as sq
-with sq.connect("blog.db") as con:
-    c = con.cursor()
-    c.execute(" CREATE TABLE posts(title TEXT, post TEXT)")
-    c.execute('INSERT INTO posts VALUES("Good","I\'m good.")')
-    c.execute('INSERT INTO posts VALUES("Well","I\'m well.")')
-    c.execute('INSERT INTO posts VALUES("Excellent","I\'m excellent.")')
-    c.execute('INSERT INTO posts VALUES("Okay","I\'m okay.")')
